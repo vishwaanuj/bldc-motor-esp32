27bf1504125aae1f961a2c5d95dfbfed425e8594
Vishwa Anuj <vishwaanuj@gmail.com>
2021-05-20 10:45:08 +0530

    Update esp_send_dummy.ino

--- esp_send_dummy/esp_send_dummy.ino
+++ esp_send_dummy/esp_send_dummy.ino
@@ -4,21 +4,21 @@
 #include <PubSubClient.h>
 
  
-#define WIFISSID "Vish" // Put your WifiSSID here
-#define PASSWORD "12345678" // Put your wifi password here
-#define TOKEN "BBFF-dCDgdF6WUWvVzYZslI1oVawmTwqeRB" // Put your Ubidots' TOKEN
+#define WIFISSID "*******" // Put your WifiSSID here
+#define PASSWORD "*********" // Put your wifi password here
+#define TOKEN "BBFF-***********************" // Put your Ubidots' TOKEN
 #define MQTT_CLIENT_NAME "ESP32_bldc_readings" // MQTT client Name, please enter your own 8-12 alphanumeric character ASCII string;
  
 /****************************************
   Define Constants
 ****************************************/
-#define VARIABLE_LABEL1 "Current Readings" // Assing the variable label
+#define VARIABLE_LABEL1 "Current Readings" // Assing the variable label 
 
-#define DEVICE_LABEL "ESP32"
+#define DEVICE_LABEL "ESP32_bldc"
  
-char mqttBroker[]  = "industrial.api.ubidots.com";
-char payload[1000];
-char topic1[150];
+char mqttBroker[]  = "industrial.api.ubidots.com";//broker of messages
+char payload[1000]; // telling the size of payoad
+char topic1[150];// allocating the topic size
 
  
 // Space to store values to send
@@ -31,9 +31,32 @@ char str_BLDC[10];
 WiFiClient ubidots;
 PubSubClient client(ubidots);
  
- 
+  
+
 void callback(char* topic, byte* payload, unsigned int length)
 {
+ 
+ /*
+related to pubsubclient
+
+
+Subscription Callback
+If the client is used to subscribe to topics, a callback function must be provided in the constructor. 
+This function is called when new messages arrive at the client.
+
+The callback function has the following signature:
+
+void callback(const char[] topic, byte* payload, unsigned int length)
+Parameters
+topic const char[] - the topic the message arrived on
+payload byte[] - the message payload
+length unsigned int - the length of the message payload
+Internally, the client uses the same buffer for both inbound and outbound messages. After the callback function returns, 
+or if a call to either publish or subscribe is made from within the callback function, the topic and payload values passed to 
+the function will be overwritten. The application should create its own copy of the values if they are required after the callback 
+returns.
+
+*/
   char p[length + 1];
   memcpy(p, payload, length);
   p[length] = NULL;
@@ -41,7 +64,9 @@ void callback(char* topic, byte* payload, unsigned int length)
   Serial.write(payload, length);
   Serial.println(topic);
 }
- 
+
+
+
 void reconnect()
 {
   // Loop until we're reconnected
@@ -64,7 +89,8 @@ void reconnect()
 }
  
 /****************************************
-  Main Functions
+  Main Functions 
+
 ****************************************/
  
 void setup()
@@ -72,31 +98,41 @@ void setup()
   Serial.begin(115200);
   while (!Serial);
 
- 
-  // Set up oversampling and filter initialization
 
+  //-----Wifi connection-----
   WiFi.begin(WIFISSID, PASSWORD);
   Serial.println();
   Serial.print("Waiting for WiFi Connection ..............");
+ 
+ //check if wifi connected if not keep on trying 
   while (WiFi.status() != WL_CONNECTED)
   {
     Serial.print(".");
     delay(500);
   }
+ //-------------------------
+ 
+ //after wifi is connected
   Serial.println("");
   Serial.println("WiFi Connected");
   Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  client.setServer(mqttBroker, 1883);
-  client.setCallback(callback);
+  Serial.println(WiFi.localIP()); //printing local wifi ip address
+  client.setServer(mqttBroker, 1883);//this port is fixed acc. to the udibots docs 
+  client.setCallback(callback); //calling the function callback for starting the publishing task
+ //----------------
 }
  
 void loop()
 {
+ 
+ // checking for the mqtt connection if not then keep on trying 
   if (!client.connected())
   {
     reconnect();
   }
+ //---------------------------
+ 
+ //------sending dummy data in for loop------
   for (int i=0;i<15;i++){
   float current = i;
   dtostrf(current, 4, 2, str_BLDC);
@@ -108,8 +144,9 @@ void loop()
   client.publish(topic1, payload);
   Serial.println();
   client.loop();
-  delay(2000);
+  delay(2000); //you change this 2 sec to 1sec(minimum can not cros beyond this) safeside 2sec
   }
+ //------------------------------------------
 
   
 }
